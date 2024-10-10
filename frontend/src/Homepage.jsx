cef481b80fc684f557407c36aec46f30dffa94e1
Pitupoom <pitupoomatom139@gmail.com>
2024-10-10 18:31:23 -0500

    Added Navbar, Font, Icon, Images

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -3,12 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function Homepage() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
+      <Navbar />
       <Hero />
     </div>
   )
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <div className='nav-container'>
+        <Link to="/" className="element" id='name'>HandyHelper&nbsp;<i class="fas fa-wrench"></i></Link>
+        <Link to="/" className='element'>ABOUT US</Link>
+        <Link to="/" className='element'>POST A REQUEST</Link>
+        <Link to="/" className='element'>BROWSE HANDYMEN</Link>
+        <Link to="/" className='element'>SIGN IN</Link>
+    </div>
+  )
+}
+
+export default Navbar
