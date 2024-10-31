4e39007ee1aee60b1c9d93d4a7afc75216be9fef
Bhumsitt Pramuanpornsatid <32533504+Bosebcc@users.noreply.github.com>
2024-10-31 16:28:26 -0500

    add get api to retrieve list of job posting | add page to display job postings

--- backend/handyhelper/urls.py
+++ backend/handyhelper/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from core.views import job_posting_list
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("core.urls")),
+    path("job-postings/", include("core.urls")),
+    path("job-postings/jobs/", job_posting_list, name='job-posting-list'),
 ]
