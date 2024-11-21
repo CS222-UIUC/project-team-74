c1bed8b25f4f7c7930359e21c92efcff323e98c9
MATUPOOM SOONTORNTHANON <matumc@hotmail.com>
2024-11-21 16:49:26 -0600

    add effect to job postings page

--- frontend/src/components/PostedJob.jsx
+++ frontend/src/components/PostedJob.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './Hero_Homepage.css';
+import './PostedJob.css';
 
 const PostedJob = () => {
   const [jobs, setJobs] = useState([]);
@@ -13,9 +13,26 @@ const PostedJob = () => {
       .catch(error => {
         console.error('There was an error fetching the job postings!', error);
       });
-
-
   }, []);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show3');
+        } else {
+          entry.target.classList.remove('show3');
+        }
+      });
+    }, {
+      
+    });
+
+    const hiddenElements = document.querySelectorAll('.hidden3'); 
+    hiddenElements.forEach((el) => observer.observe(el));
+
+    return () => observer.disconnect();
+  }, [jobs]);  
 
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
@@ -26,27 +43,27 @@ const PostedJob = () => {
           entry.target.classList.remove('show2');
         }
       });
+    }, {
+      
     });
 
     const hiddenElements = document.querySelectorAll('.hidden2'); 
     hiddenElements.forEach((el) => observer.observe(el));
 
-  
     return () => observer.disconnect();
-  }, []);
-
+  }, [jobs]);  
 
   return (
     <div className="max-w-full max-h-full pt-10 pr-10 pb-8 pl-10 bg-white">
       <h1 className="text-[3.5rem] font-bold mb-6 text-center text-green-700 font-fraunces hidden2">
         Job Postings
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2  gap-6 pt-2 pl-10 pr-10 pb-20 ">
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 pt-2 pl-10 pr-10 pb-20">
         {jobs.map(job => (
           <div
             key={job.id}
-            className="bg-white p-6 rounded-xl shadow-lg hover:shadow-2xl transition duration-300 border border-gray-200  "
-           
+            className="bg-white p-6 rounded-xl shadow-lg hover:shadow-2xl transition duration-300 border border-gray-200 hidden3"  // hidden2 class applied here
           >
             <h2 className="text-2xl font-bold mb-2 text-[#b38600]">
               {job.title}
@@ -59,8 +76,6 @@ const PostedJob = () => {
       </div>
     </div>
   );
-  
-  
 };
 
 export default PostedJob;
