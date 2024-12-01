7b125a728b39af934e5c1920f7d50b6c377145bb
Bhumsitt Pramuanpornsatid <32533504+Bosebcc@users.noreply.github.com>
2024-12-01 17:23:43 -0600

    Remove unused useEffect import from Job_Post_Form component

--- frontend/src/components/Job_Post_Form.jsx
+++ frontend/src/components/Job_Post_Form.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './Job_Post_Form.css';
 import { MdOutlineTitle, MdSubtitles } from "react-icons/md";
